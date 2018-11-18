adf8dcc4fb0787714a567900da1a852162905317
iggprt <33717256+iggprt@users.noreply.github.com>
2018-11-18 09:11:11 +0200

    night

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url,include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^personal/', include('personal.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^webapp/', include('webapp.urls')),
     url(r'^$', include('webapp.urls')),
